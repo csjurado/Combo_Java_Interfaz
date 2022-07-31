7246a3d3fce63416e9570cf40f9f767c91b2cf38
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2022-07-30 19:30:08 -0500

    ACTUALIZACION LISTO Y COMPLETADO 5 CONVERSIONES DE IZQUIERA A DERECHA Y DE DERECHA A IZQUIERDA USO DEL origenCB.getSelectedItem().toString()

--- src/monedas.java
+++ src/monedas.java
@@ -12,22 +12,57 @@ public class monedas {
     private JLabel destinoTXT;
     private JLabel auxiliarTXT;
     private JPanel mainPanel;
+    private JLabel auxiliar2TXT;
 
 
     public monedas() {
         origenCB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            auxiliarTXT.setText(""+origenCB.getSelectedItem());
+                auxiliarTXT.setText(""+origenCB.getSelectedItem());
             }
         });
+        destinoCB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                auxiliar2TXT.setText(""+destinoCB.getSelectedItem());
+            }
+        });
+
         convertirBTN.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double [] tarifas= {2046.03,447400,100,96.37};
-                double tarifasSeleccionada=tarifas[destinoCB.getSelectedIndex()];
-                double resultado = Double.parseDouble(origenTF.getText())*tarifasSeleccionada/100;
-                destinoTF.setText(""+resultado);
+
+                if (origenCB.getSelectedItem().toString()=="Dólares"){
+                    double [] tarifas= {2036.79,447400,100,96.37,82.14};
+                    double tarifasSeleccionada=tarifas[destinoCB.getSelectedIndex()];
+                    double resultado = Double.parseDouble(origenTF.getText())*tarifasSeleccionada/100;
+                    destinoTF.setText(String.format("%.2f",resultado));
+                }
+                if((origenCB.getSelectedItem().toString()=="Pesos (MX)")){
+                    double [] tarifas= {100000000,21060985500.00,4909698.30,4674923.40,4032607.21};
+                    double tarifasSeleccionada=tarifas[destinoCB.getSelectedIndex()];
+                    double resultado = Double.parseDouble(origenTF.getText())*tarifasSeleccionada/100000000;
+                    destinoTF.setText(String.format("%.2f",resultado));
+                }
+                if((origenCB.getSelectedItem().toString()=="Pesos (Col)")){
+                    double [] tarifas= {465300.89,100,22844.87,21752,46,18763.76};
+                    double tarifasSeleccionada=tarifas[destinoCB.getSelectedIndex()];
+                    double resultado = Double.parseDouble(origenTF.getText())*tarifasSeleccionada/100000000;
+                    destinoTF.setText(String.format("%.2f",resultado));
+                }
+                if((origenCB.getSelectedItem().toString()=="Franco Suizo")){
+                    double [] tarifas= {2139.07,450509.72,105.02,100,86.26};
+                    double tarifasSeleccionada=tarifas[destinoCB.getSelectedIndex()];
+                    double resultado = Double.parseDouble(origenTF.getText())*tarifasSeleccionada/100;
+                    destinoTF.setText(String.format("%.2f",resultado));
+                }
+                if((origenCB.getSelectedItem().toString()=="Libra Esterlina")){
+                    double [] tarifas= {2479.78,522266.89,121.75,115.93,100};
+                    double tarifasSeleccionada=tarifas[destinoCB.getSelectedIndex()];
+                    double resultado = Double.parseDouble(origenTF.getText())*tarifasSeleccionada/100;
+                    destinoTF.setText(String.format("%.2f",resultado));
+                }
             }
         });
     }
@@ -36,7 +71,7 @@ public class monedas {
         JFrame frame = new JFrame("monedas");
         frame.setContentPane(new monedas().mainPanel);
         frame.setTitle("CONVERTIDOR DE MONEDAS");
-        frame.setSize(700,600);
+        frame.setSize(600,400);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
